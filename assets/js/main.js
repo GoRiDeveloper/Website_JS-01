4c8b094c745a1b7cc522ad10ca63f8f8fa06a956
GoRiJ <jolem824@gmail.com>
2022-05-24 15:44:14 -0500

    beta-01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
-alert("Hola");
+let inputText = prompt("Ingresa un Nómbre Registrado: ");
+
+while (inputText === "ESC"){
+
+    alert("El usuario " + inputText + " esta registrado.");
+
+    break;
+
+}
+
+while (inputText != "ESC"){
+
+    for (let i = 1; i <= 3; i++) {
+
+        alert("Intento N° " + i + " de 3. El usuario " + inputText + " no esta registrado.");
+        
+        inputText = prompt("Ingresa otro usuario");
+
+    }
+
+    break;
+
+}
